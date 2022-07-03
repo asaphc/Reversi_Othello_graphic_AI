36bc06be67b61ddf85feb11078447f674b0bc058
asaphc <asaphcohenq@gmail.com>
2022-07-03 14:11:44 +0300

    update

--- Reversi/PlayGame.py
+++ Reversi/PlayGame.py
@@ -1,7 +1,18 @@
 import Play
 import Alpha_Beta_Agent
 
-game = Play.Play(8,  agent2 = Alpha_Beta_Agent.AlphaBetaAgent(-1,2))
+#####
+# running a game, the first parameter is the board size.
+# two add AI add
+# agent1 = Agent... for an AI for player 1
+# and 
+# agent2 = ...
+# for an AI for player 2
+# you can also have an AI for both
+# example:
+# Play.Play(8,  agent2 = Alpha_Beta_Agent.AlphaBetaAgent(-1,2))
+
+Play.Play(8,  agent2 = Alpha_Beta_Agent.AlphaBetaAgent(-1,2))
 
 
 
